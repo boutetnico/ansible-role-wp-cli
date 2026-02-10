ff5c434cf5175fddc0d614e63df480604fa9f4f6
Nicolas Boutet <amd3002@gmail.com>
2026-02-10 09:01:07 +0100

    Improve test coverage

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -8,9 +8,22 @@ import pytest
     ],
 )
 def test_wp_cli_is_installed(host, path, username, groupname, mode):
-    path = host.file(path)
-    assert path.exists
-    assert path.is_file
-    assert path.user == username
-    assert path.group == groupname
-    assert path.mode == mode
+    f = host.file(path)
+    assert f.exists
+    assert f.is_file
+    assert f.user == username
+    assert f.group == groupname
+    assert f.mode == mode
+
+
+def test_wp_cli_is_executable(host):
+    cmd = host.run("wp --version --allow-root")
+    assert cmd.rc == 0
+    assert "WP-CLI" in cmd.stdout
+
+
+def test_wp_cli_info_command(host):
+    cmd = host.run("wp cli info --allow-root")
+    assert cmd.rc == 0
+    assert "PHP binary" in cmd.stdout
+    assert "WP-CLI root dir" in cmd.stdout
